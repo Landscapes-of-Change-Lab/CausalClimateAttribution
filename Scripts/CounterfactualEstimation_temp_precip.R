20784018609953c8dcf98643ce5421fcf686698d
Joan Dudney <jdudney@berkeley.edu>
2025-02-26 15:46:52 -0800

    Updates to counterfactual scenario scripts

---
+++ Scripts/CounterfactualEstimation_temp_precip.R
@@ -0,0 +1,317 @@
+## ---------------------------
+##
+## Script name: Counterfactual estimate
+##
+## Author: Dr. Joan Dudney
+##
+## Date Created: 2024-08-02
+##
+## Copyright (c) Joan Dudney, 2024
+## Email: dudney@ucsb.edu
+##
+## ---------------------------
+##
+## Notes:
+##   This script estimates the counterfactual climate and growth scenarios
+##
+## ---------------------------
+
+
+## packages
+librarian::shelf(sjPlot, ggeffects, patchwork, tidyverse, broom,progress,
+                 lme4, plotrix, ggpubr, mgcv, nlme, fixest, plotrix, egg, ggpmisc,
+                 mvtnorm, clubSandwich, rasterVis, broom.mixed, scales,RColorBrewer, splines, zoo)
+
+
+## data
+rwldat <- read_csv(here("Data", "Primary_data", "paneldat_RWL.csv"))
+
+
+
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+#
+#
+#                    COUNTERFACTUAL TEMPERATURE SCENARIO
+#
+#
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+## estimate changes in maximum temperature as a function of year for each plot
+## use a flexible spline to improve predictive accuracy
+
+## temperature (hereafter tmax)
+temp_mod <- lm(tmax ~ ns(year, df = 4) + factor(plot_id_needle), data=rwldat)
+summary(temp_mod)
+
+## precipitation (hereafter precip)
+precip_mod <- lm(ppt ~ ns(year, df = 4) + factor(plot_id_needle), data=rwldat)
+summary(precip_mod)
+
+
+## create a new dataset and add predicted temperature
+preddat <- rwldat
+preddat$predtmax <- predict(temp_mod)
+preddat$predppt <- predict(precip_mod)
+
+## visualize the predicted data compared to observed data
+
+## temp
+preddat %>% 
+  ggplot(aes(x=year, y=tmax, color="tmax"))+
+  geom_line()+
+  geom_line(aes(y=predtmax, color="predtmax"))+
+  geom_line()
+
+## precip
+preddat %>% 
+  ggplot(aes(x=year, y=ppt, color="precip"))+
+  geom_line()+
+  geom_line(aes(y=predppt, color="predprecip"))+
+  geom_line()
+
+## calculate plot-level mean tmax
+meanpre80 <- preddat %>% 
+  filter(year<1980) %>% 
+  group_by(plot_id_needle) %>% 
+  summarize(meantmax = mean(tmax), meanppt = mean(ppt)) 
+
+## join plot means with main dataset
+mean_counterdat <- preddat %>% 
+  left_join(meanpre80)
+
+## add year-level heterogeneity to the counterfactual scenario
+## formula for each predicted tmax: (tmax - predtmax) + meantmax
+## time period 1980-2018
+counterdat80 <- mean_counterdat %>% 
+  mutate(tmax_counter = (tmax - predtmax) + meantmax) %>% ## adding heterogeneity
+  mutate(tmax_counter_historic = ifelse(year>1979, tmax_counter, tmax)) %>%  ## include counterfactual values after 1979 only
+  mutate(ppt_counter = (ppt - predppt) + meanppt) %>% 
+  mutate(ppt_counter_historic = ifelse(year>1999, ppt_counter, ppt))
+
+
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# FIGURE 6A
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+## visualize difference between actual and counterfactual scenarios
+counter_clim_fig <- counterdat80 %>% 
+  group_by(year) %>% 
+  summarize(meantmax = mean(tmax), meancounter = mean(tmax_counter_historic)) %>% 
+  select(year, meantmax, meancounter) %>% 
+  pivot_longer(-year) %>% 
+  ggplot(aes(x=year, y=value, color=name))+
+  geom_line(linewidth = .5, alpha=.9)+
+  geom_point(size = .9, alpha=.9)+
+  geom_smooth(se=F, span=1)+
+  labs(color = "",
+       x = "Year",
+       y = "Temperature (°C)")+
+  scale_color_manual(
+    values = c( "meantmax" = "#C75B77" , "meancounter" = "#303077"),
+    labels = c("Counterfactual", "Actual"))+
+  theme(legend.position=c(.70,.90),
+        legend.background = element_blank())
+
+counter_clim_fig
+
+
+
+## precipitation figure
+counterdat80 %>% 
+  group_by(year) %>% 
+  summarize(meanppt = mean(ppt), meancounter = mean(ppt_counter_historic)) %>% 
+  select(year, meanppt, meancounter) %>% 
+  pivot_longer(-year) %>% 
+  ggplot(aes(x=year, y=value, color=name))+
+  geom_line(linewidth = .5, alpha=.9)+
+  geom_point(size = .9, alpha=.9)+
+  geom_smooth(se=F, span=1)+
+  labs(color = "",
+       x = "Year",
+       y = "Precipitation (mm)")+
+  scale_color_manual(
+    values = c( "meanppt" = "#C75B77" , "meancounter" = "#303077"),
+    labels = c("Counterfactual", "Actual"))+
+  theme(legend.position=c(.70,.90),
+        legend.background = element_blank())
+
+
+
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+#
+#
+#                   ESTIMATING THE DIFFERENT COUNTERFACTUAL GROWTH SCENARIOS
+#
+#
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+## rename data
+paneldat <- counterdat80
+
+## extracting coefficient matrix
+fe_mod <-  feols(log(value+0.01) ~ tmax * ppt + year | tree_id,
+                 data= paneldat, cluster = ~ plot_id_needle)
+
+summary(fe_mod)
+
+V_CR1 = vcov(fe_mod)
+V_CR1=as.matrix(V_CR1)
+coef_vector = fe_mod$coefficients
+
+
+## randomly drawing coefficients from distribution
+draw = rmvnorm(n = 1000, mean = coef_vector, sigma = V_CR1)
+
+
+## creating counterfactual and actual dataframes
+counterdat <- counterdat80 %>% 
+  select(plot_id_needle, tmax_counter_historic, ppt, tree_id, year) %>% 
+  rename(tmax = tmax_counter_historic)
+
+actualdat <- counterdat80 %>% 
+  select(plot_id_needle, tmax, ppt, tree_id, year)
+
+
+
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# MONTE CARLO SIMULATION
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+## create an empty dataframe for results
+results=data.frame()
+
+pb <- progress_bar$new(
+  format = "  Processing [:bar] :percent eta: :eta",
+  total = 1000,
+  clear = FALSE,
+  width = 60
+)
+
+
+for (i in 1:1000){
+  
+  pb$tick()
+  
+  d <- draw[i,]
+  modified_fe_mod <-  fe_mod
+  modified_fe_mod$coefficients <- d
+  actualdat$vals_actual <-  predict(modified_fe_mod, newdata = actualdat)
+  actualdat$vals_counter <-  predict(modified_fe_mod, newdata = counterdat)
+  
+  results_dat_1980 <- actualdat %>% 
+    filter(year>1979) %>% 
+    mutate(tree_diff = vals_actual - vals_counter) %>% 
+    mutate(iteration = i, period = "1980-2018")
+  
+  results_dat_2000 <- actualdat %>% 
+    filter(year>1999) %>% 
+    mutate(tree_diff = vals_actual - vals_counter) %>% 
+    mutate(iteration = i, period = "2000-2018")
+  
+  results_dat_2010 <- actualdat %>% 
+    filter(year>2009) %>% 
+    mutate(tree_diff = vals_actual - vals_counter) %>% 
+    mutate(iteration = i, period = "2010-2018")
+  
+  results <- rbind(results, results_dat_1980, results_dat_2000, results_dat_2010)
+}
+
+head(results)
+
+
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# THE FIGURES
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+## new dataframe to create figures from
+pred_results <- results
+#write_csv(pred_results, here("Data", "Primary_data", "MC_simulation_data.csv"))
+
+set.seed(123)  # for reproducibility
+tercile_boundaries <- quantile(pred_results$ppt, probs = c(1/3, 2/3))
+
+# First, let's prepare the data
+calc_terc <- pred_results %>%
+  sample_n(10000) %>% 
+  mutate(precip_terciles = case_when(
+    ppt < 0.667222 ~ "0-33.3%",
+    ppt >= 0.667222 & ppt < 1.045141 ~ "33.4-66.7%",
+    TRUE ~ "67.1-100%"
+  ))
+
+
+# Calculate the mean and 95% CI for each year and tercile
+summarized_data <- calc_terc%>%
+  group_by(year, precip_terciles) %>%
+  summarise(
+    mean_diff = mean(tree_diff),
+    se = sd(tree_diff) / sqrt(n()),
+    lower_ci = mean_diff - 1.96 * se,
+    upper_ci = mean_diff + 1.96 * se,
+    .groups = "drop"
+  )
+
+# Figure
+ribbonplot = ggplot(summarized_data, aes(x = year, y = tree_diff, color = precip_terciles, fill = precip_terciles)) +
+  geom_hline(yintercept = 0, linetype = "dashed") +
+  #geom_point(alpha = 0.01) +
+  geom_ribbon(data = summarized_data, 
+              aes(y = mean_diff, ymin = lower_ci, ymax = upper_ci),
+              alpha = 0.2, color = NA) +
+  geom_line(data = summarized_data, aes(y = mean_diff), size = 1) +
+  scale_color_manual(values = c("0-33.3%" = "#303077", "33.4-66.7%" = "#A4BDD7", "67.1-100%" = "#8c6e87")) +
+  scale_fill_manual(values = c("0-33.3%" = "#303077", "33.4-66.7%" = "#A4BDD7", "67.1-100%" = "#8c6e87")) +
+  labs(y = "Δ log(RWL)", x = "Year", color = "Precip. terciles", fill = "Precip. terciles") +
+  theme(legend.position = c(.2,.65))+
+  theme(
+    legend.background = element_rect(fill = "transparent"), 
+    legend.key = element_rect(fill = "transparent", color = NA) 
+  )
+
+ribbonplot
+
+
+
+precipterc <- pred_results %>% 
+  mutate(precip_terciles = case_when(
+    ppt < 0.667222 ~ "0-33.3%",
+    ppt >= 0.667222 & ppt < 1.045141 ~ "33.4-66.7%",
+    TRUE ~ "67.1-100%")) %>% 
+  group_by(period, precip_terciles) %>% 
+  summarize(meandiff = mean(tree_diff),lowCI = quantile(tree_diff, probs = .025), highCI = quantile(tree_diff, probs = 0.95)) %>% 
+  ungroup()
+
+
+# Define the color palette
+color_palette <- c("0-33.3%" = "#303077", "33.4-66.7%" = "#A4BDD7", "67.1-100%" = "#8c6e87")
+
+diff_fig <- precipterc %>% 
+  ggplot(aes(x = factor(period), y = meandiff, color = precip_terciles, fill = precip_terciles)) +
+  geom_point(size = 3) +
+  geom_hline(yintercept = 0, linetype = "dashed") +
+  geom_errorbar(aes(ymin = lowCI, ymax = highCI), width = 0.1) +
+  facet_grid(~precip_terciles) +
+  labs(title = "Estimated climate change impacts",
+       x = "Time period",
+       y = "Δ log(RWL)") +
+  scale_color_manual(values = color_palette) +
+  scale_fill_manual(values = color_palette) +
+  guides(fill=F, color=F)+
+  theme(axis.text.x = element_text(angle = 0, hjust = 0.5))+
+  theme(axis.text.x = element_text(angle = 45, hjust = .4, vjust = 0.5),
+        plot.title = element_text(hjust = 0.5))
+
+diff_fig
+
+
+
+theme_set(
+  theme_bw(base_size = 20)+
+    theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())
+)
+
+
+counter_clim_fig + (ribbonplot/diff_fig) & plot_annotation(tag_levels = "A")
+
+
+
--- Scripts/MarginalEffectsPlot.R
+++ Scripts/MarginalEffectsPlot.R
@@ -28,8 +28,8 @@ theme_set(
 
 
 ## data
-rwldat <- read_csv(here("Data", "paneldat_RWL.csv"))
-climdat <- read_csv(here("Data", "climatewindows.csv"))
+rwldat <- read_csv(here("Data", "Primary_data", "paneldat_RWL.csv"))
+climdat <- read_csv(here("Data","Primary_data", "climatewindows.csv"))
 
 paneldat <- rwldat %>% 
   left_join(climdat)
@@ -62,6 +62,7 @@ mtemp <- slopes(fe_mod,
 marg_temp = ggplot(mtemp, aes(x = ppt*1000, y = estimate, color)) +
   geom_hline(yintercept = 0, linetype="dashed")+
   geom_line(color="#91376F") +
+  ylim(-.03, .08)+
   geom_ribbon(aes(ymin = conf.low, ymax = conf.high), alpha = 0.2, color="#91376F", fill="#91376F") +
   labs(x = "Precipitation (mm)", 
        y = "Marginal effect of temperature")+
@@ -78,6 +79,7 @@ mprecip <- slopes(fe_mod,
 marg_precip = ggplot(mprecip, aes(x = tmax, y = estimate)) +
   geom_hline(yintercept = 0, linetype="dashed")+
   geom_line(color= "#303077") +
+  ylim(-.2, .45)+
   geom_ribbon(aes(ymin = conf.low, ymax = conf.high), alpha = 0.2, color="#303077", fill = "#303077") +
   labs(x = "Temperature (°C)", 
        y = "Marginal effect of precipitation")
@@ -86,4 +88,101 @@ marg_precip = ggplot(mprecip, aes(x = tmax, y = estimate)) +
 
 marg_temp + marg_precip +   plot_annotation(tag_levels = "A")
  
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# Adding density distribution panels to both plots
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+# Add labels A and B to the density plots only
+ppt_density <- ggplot(paneldat, aes(x = ppt*1000)) +
+  geom_density(fill = "#91376F", alpha = 0.6) +
+  scale_x_continuous(breaks = seq(0, 3800, by = 500)) +
+  scale_y_continuous(
+    breaks = function(x) pretty(x, n = 3),
+    labels = function(x) format(x, scientific = FALSE)  # Add this line
+  ) +
+  ggtitle("A") +
+  theme(
+    axis.title.x = element_blank(),
+    axis.text.x = element_blank(),
+    plot.margin = margin(5, 0, -5, 0),  # Negative bottom margin to reduce gap
+    panel.border = element_blank(),
+    axis.line.x = element_line(color = "gray70"),
+    axis.line.y = element_line(color = "gray70"),
+    plot.title = element_text(size = 16, face = "bold", hjust = 0, vjust = 1),
+    plot.title.position = "plot"
+  ) +
+  labs(y = "Density")
+
+
+# Adjust the temperature margins plot
+marg_temp <- marg_temp +
+  theme(plot.margin = margin(0, 0, 0, 0))
+
+# Combine with temperature marginal effects plot
+combined_temp_plot <- ppt_density / marg_temp +
+  plot_layout(heights = c(2, 5), guides = "collect") +
+  plot_annotation(theme = theme(plot.margin = margin(5, 5, 5, 5))) &
+  theme(plot.margin = margin(5, 5, 5, 5))
+
+# Add label B to the temperature density plot
+tmax_density <- ggplot(paneldat, aes(x = tmax)) +
+  geom_density(fill = "#303077", alpha = 0.6) +
+  scale_y_continuous(breaks = function(x) pretty(x, n = 3)) +
+  ggtitle("B") +
+  theme(
+    axis.title.x = element_blank(),
+    axis.text.x = element_blank(),
+    plot.margin = margin(5, 0, -5, 0),  # Negative bottom margin to reduce gap
+    panel.border = element_blank(),
+    axis.line.x = element_line(color = "gray70"),
+    axis.line.y = element_line(color = "gray70"),
+    plot.title = element_text(size = 16, face = "bold", hjust = 0, vjust = 1),
+    plot.title.position = "plot"
+  ) +
+  labs(y = "Density")
+
+# Adjust the precipitation margins plot
+marg_precip <- marg_precip +
+  theme(plot.margin = margin(0, 0, 0, 0))
+
+# Combine with precipitation marginal effects plot
+combined_precip_plot <- tmax_density / marg_precip +
+  plot_layout(heights = c(2, 5), guides = "collect") +
+  plot_annotation(theme = theme(plot.margin = margin(5, 5, 5, 5))) &
+  theme(plot.margin = margin(5, 5, 5, 5))
+
+# Display the combined plots
+# combined_temp_plot
+# combined_precip_plot
+
+# Set top margin of margin plots to negative to reduce gap
+marg_temp <- marg_temp +
+  theme(
+    plot.margin = margin(-5, 0, 5, 0)  # Negative top margin to reduce gap
+  )
+
+# Set top margin of precipitation plot to negative to reduce gap
+marg_precip <- marg_precip +
+  theme(
+    plot.margin = margin(-5, 0, 5, 0)  # Negative top margin to reduce gap
+  )
+
+# Combine with minimal spacing between panels
+combined_temp_plot <- ppt_density / marg_temp +
+  plot_layout(heights = c(1.5, 5)) & 
+  theme(plot.margin = margin(0, 5, 0, 5))
+
+combined_precip_plot <- tmax_density / marg_precip +
+  plot_layout(heights = c(1.5, 5)) & 
+  theme(plot.margin = margin(0, 5, 0, 5))
+
+# Wrap each combined plot without additional theming
+wrapped_temp_plot <- combined_temp_plot
+wrapped_precip_plot <- combined_precip_plot
+
+# Combine the wrapped plots
+final_figure <- wrapped_temp_plot | wrapped_precip_plot
+
+# Display the final combined figure
+final_figure
 
