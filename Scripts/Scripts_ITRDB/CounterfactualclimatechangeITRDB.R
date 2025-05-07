c5f3843df6fc4b4f4cad0c72d81caa841c0872a9
Joan Dudney <jdudney@berkeley.edu>
2025-05-07 16:17:31 -0700

    updates to the counterfactual scripts

--- Scripts/Scripts_ITRDB/CounterfactualclimatechangeITRDB.R
+++ Scripts/Scripts_ITRDB/CounterfactualclimatechangeITRDB.R
@@ -54,7 +54,7 @@ treedat <- paneldat %>%
 itrdbdat <- read_csv(here("Data", "ITRDB_species_latlon.csv")) %>%
   filter(species_id == "pied") 
 
-## isolating sites for PIED
+## isolating sites of PIED
 pied <- itrdbdat %>% 
   select(species_id, collection_id) %>% 
   distinct()
@@ -78,6 +78,9 @@ pied_clim <- prism_itrdb %>%
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+
+#========== Subsetting and further cleaning data ==========
+
 ## all climate data
 climdat <- pied_clim %>% 
   mutate(growing=ifelse(month%in%c(10:12), year+1, year)) %>% 
@@ -90,53 +93,52 @@ climdat <- pied_clim %>%
   ungroup() %>% 
   rename(plot = collection_id)
 
+climdat_short <- climdat %>% 
+  filter(year>1979)
+
+clim_means <- climdat %>%
+  filter(year < 1979) %>%
+  group_by(plot) %>%
+  summarize(hist_mtmax = mean(tmax),
+            hist_mppt = mean(ppt))
+
+climdat_short <- climdat_short %>%
+  left_join(clim_means, by = "plot") %>%
+  mutate(tmax_anom = tmax - hist_mtmax,
+         ppt_anom = ppt - hist_mppt,
+         plus_year = year - 1980)
+
+#========== Climate models ==========
+
 ## temperature model
-temp_mod <- lm(tmax ~ ns(year, df = 4) + factor(plot), data=climdat)
+temp_mod <- lm(tmax_anom ~ plus_year + 0, data=climdat_short)
 summary(temp_mod)
 
 ## precipitation model
-precip_mod <- lm(ppt ~ ns(year, df = 1) + factor(plot), data=climdat)
+precip_mod <- lm(ppt_anom ~ plus_year + 0, data=climdat_short)
 summary(precip_mod)
 
-## create a new dataset and add predicted temperature
-preddat <- climdat
-preddat$predtmax <- predict(temp_mod)
-preddat$predppt <- predict(precip_mod)
+## counterfactual climate data
+climdat_short <- climdat_short %>%
+  mutate(tmax_pred_anom = predict(temp_mod),
+         ppt_pred_anom = predict(precip_mod),
+         tmax_cf = tmax - tmax_pred_anom,
+         ppt_cf = ppt - ppt_pred_anom)
 
-# ## visualize the predicted data compared to observed data
-# 
-# ## temp
-# preddat %>% 
-#   ggplot(aes(x=year, y=tmax, color="tmax"))+
-#   geom_line()+
-#   geom_line(aes(y=predtmax, color="predtmax"))+
-#   geom_line()
-# 
-# ## precip
-# preddat %>% 
-#   ggplot(aes(x=year, y=ppt, color="precip"))+
-#   geom_line()+
-#   geom_line(aes(y=predppt, color="predprecip"))+
-#   geom_line()
 
-## calculate plot-level mean tmax
-meanpre80 <- preddat %>% 
-  filter(year<1980) %>% 
-  group_by(plot) %>% 
-  summarize(meantmax = mean(tmax), meanppt = mean(ppt)) 
+#========== Counterfactual datasets ==========
 
-## join plot means with main dataset
-mean_counterdat <- preddat %>% 
-  left_join(meanpre80)
+## creating counterfactual and actual dataframes
+counterdat <- climdat_short %>%
+  select(plot, tmax_cf, ppt, year) %>%
+  rename(tmax = tmax_cf) %>%
+  left_join(treedat)%>% 
+  filter(year<2014)
 
-## add year-level heterogeneity to the counterfactual scenario
-## formula for each predicted tmax: (tmax - predtmax) + meantmax
-## time period 1980-2018
-counterdat80 <- mean_counterdat %>% 
-  mutate(tmax_counter = (tmax - predtmax) + meantmax) %>% ## adding heterogeneity
-  mutate(tmax_counter_historic = ifelse(year>1979, tmax_counter, tmax)) %>%  ## include counterfactual values after 1979 only
-  mutate(ppt_counter = (ppt - predppt) + meanppt) %>% 
-  mutate(ppt_counter_historic = ifelse(year>1979, ppt_counter, ppt))
+actualdat <- climdat_short%>%
+  left_join(treedat) %>% 
+  select(plot, tmax, ppt, year, tree) %>% 
+  filter(year<2014)
 
 
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -164,20 +166,6 @@ coef_vector = fe_mod$coefficients
 draw = rmvnorm(n = 1000, mean = coef_vector, sigma = V_CR1)
 
 
-## creating counterfactual and actual dataframes
-
-## include precip counter
-counterdat <- counterdat80 %>%
-  select(plot, tmax_counter_historic, ppt, year) %>%
-  rename(tmax = tmax_counter_historic) %>%
-  left_join(treedat)%>% 
-  filter(year<2014)
-
-actualdat <- counterdat80 %>%
-  left_join(treedat) %>% 
-  select(plot, tmax, ppt, year, tree) %>% 
-  filter(year<2014)
-
 
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 # MONTE CARLO SIMULATION
@@ -194,7 +182,7 @@ pb <- progress_bar$new(
 )
 
 
-for (i in 1:10){
+for (i in 1:1000){
   
   pb$tick()
   
@@ -256,10 +244,7 @@ perchange <- mainresults %>%
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-
-#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-# FIGURE 6B-C
-#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+#========== Ribbon plot figure  ==========
 
 ## new dataframe to create figures from
 pred_results <- results 
@@ -279,43 +264,39 @@ tercdat <- tercdat %>%
     TRUE ~ "66.8-100%"
   ))
 
-# Sample the dataset to be able to plot it
-calc_terc <- pred_results %>%
-  left_join(tercdat)
-
-
-# Calculate the median and 95% CI for plotting 
-summarized_data <- calc_terc%>%
+summarized_data <-  pred_results %>% 
+  #filter(year>1990) %>% 
+  left_join(tercdat) %>% 
   group_by(year, precip_terciles, iteration) %>%
-  summarise(mean_diff = mean(tree_diff)) %>%
+  summarize(mean_diff = mean(tree_diff)) %>%
   group_by(year, precip_terciles) %>% 
   summarize(lower_ci = quantile(mean_diff, 0.025),
             rwi_mean = mean(mean_diff),
             upper_ci = quantile(mean_diff, 0.975))
 
 # Figure
-ribbonplot = summarized_data %>% 
-  filter(precip_terciles!="33.4-66.7%") %>% 
-  ggplot( aes(x = year, y = rwi_mean, color = precip_terciles, fill = precip_terciles)) +
+ribbonplot = ggplot(summarized_data, aes(x = year, y = tree_diff, color = precip_terciles, fill = precip_terciles)) +
   geom_hline(yintercept = 0, linetype = "dashed") +
   #geom_point(alpha = 0.01) +
-  geom_ribbon(
+  geom_ribbon(data = summarized_data, 
               aes(y = rwi_mean, ymin = lower_ci, ymax = upper_ci),
               alpha = 0.2, color = NA) +
-  geom_line( aes(y = rwi_mean), size = 1) +
+  geom_line(data = summarized_data, aes(y = rwi_mean), size = 1) +
   scale_color_manual(values = c("0-33.3%" = "#303077", "33.4-66.7%" = "#A4BDD7", "66.8-100%" = "#8c6e87")) +
   scale_fill_manual(values = c("0-33.3%" = "#303077", "33.4-66.7%" = "#A4BDD7", "66.8-100%" = "#8c6e87")) +
   labs(y = "Δ RWI", x = "Year", color = "Precip. terciles", fill = "Precip. terciles") +
-  theme(legend.position = c(-.00000001,.15))+
-  theme(
-    legend.position = c(0.22, 0.38),  
+  #theme(legend.position = c(.2,.65))+
+  theme(legend.position=c(.2,.35),
     legend.background = element_rect(fill = "transparent"), 
-    legend.key = element_rect(fill = "transparent", color = NA)
+    legend.key = element_rect(fill = "transparent", color = NA) 
   )
 
 ribbonplot
 
 
+#========== Mean change figure  ==========
+
+## data
 precipterc <- pred_results %>% 
   left_join(tercdat) %>% 
   group_by(period, precip_terciles, iteration) %>% 
@@ -325,14 +306,16 @@ precipterc <- pred_results %>%
             upper_ci = quantile(mean_diff, 0.975)) %>% 
   ungroup()
 
-# Define the color palette
+## Define the color palette
 color_palette <- c("0-33.3%" = "#303077", "33.4-66.7%" = "#A4BDD7", "66.8-100%" = "#8c6e87")
 
+
+## figure
 diff_fig <- precipterc %>% 
   ggplot(aes(x = factor(period), y = rwi_mean, color = precip_terciles, fill = precip_terciles)) +
   geom_hline(yintercept = 0, linetype = "dashed") +
-  geom_errorbar(aes(ymin = lower_ci, ymax = upper_ci, width = 0.1, color = "black"))+
-  geom_point(size = 1) +
+  geom_errorbar(aes(ymin = lower_ci, ymax = upper_ci, width = 0.07), color = "black")+
+  geom_point(size = 1.5) +
   facet_grid(~precip_terciles) +
   labs(title = "Estimated climate change impacts",
        x = "Time period",
@@ -349,16 +332,19 @@ diff_fig <- precipterc %>%
 diff_fig
 
 
-#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-# FIGURE 6A
-#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+#========== Temperature counterfactual figure ==========
 
-
+## fig temp trends
 ## visualize difference between actual and counterfactual scenarios
-counter_clim_fig <- counterdat80 %>% 
-  filter(year<2015) %>% 
+allclimdat <- climdat_short %>% 
+  select(plot, year, tmax_cf) %>% 
+  right_join(climdat) %>% 
+  mutate(tmax_cf_all = ifelse(year < 1980, tmax, tmax_cf)) %>% 
+  filter(year<2017)
+
+counter_clim_fig <- allclimdat %>% 
   group_by(year) %>% 
-  summarize(meantmax = mean(tmax), meancounter = mean(tmax_counter_historic)) %>% 
+  summarize(meantmax = mean(tmax), meancounter = mean(tmax_cf_all)) %>% 
   select(year, meantmax, meancounter) %>% 
   pivot_longer(-year) %>% 
   ggplot(aes(x=year, y=value, color=name))+
@@ -371,15 +357,16 @@ counter_clim_fig <- counterdat80 %>%
   scale_color_manual(
     values = c( "meantmax" = "#C75B77" , "meancounter" = "#303077"),
     labels = c("Counterfactual", "Actual"))+
-  theme(legend.position=c(.80,.98),
+  theme(legend.position=c(.74,.95),
         legend.background = element_blank())
 
 counter_clim_fig
 
-
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 # Combining panels
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
 
 theme_set(
   theme_bw(base_size = 20)+
@@ -394,5 +381,110 @@ counter_clim_fig + (ribbonplot/diff_fig) & plot_annotation(tag_levels = "A")
 
 
 
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+#
+#
+#                             Robustness check for precip counterfactual
+#
+#
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+## creating counterfactual and actual dataframes
+counterdat <- climdat_short %>%
+  select(plot, tmax_cf, ppt_cf, year) %>%
+  rename(tmax = tmax_cf,
+         ppt = ppt_cf) %>%
+  left_join(treedat)%>% 
+  filter(year<2014)
+
+actualdat <- climdat_short%>%
+  left_join(treedat) %>% 
+  select(plot, tmax, ppt, year, tree) %>% 
+  filter(year<2014)
 
 
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# MC Simulation with precipitation
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+## create an empty dataframe for results
+results_precip=data.frame()
+
+pb <- progress_bar$new(
+  format = "  Processing [:bar] :percent eta: :eta",
+  total = 1000,
+  clear = FALSE,
+  width = 60
+)
+
+
+for (i in 1:100){
+  
+  pb$tick()
+  
+  d <- draw[i,]
+  modified_fe_mod <-  fe_mod
+  modified_fe_mod$coefficients <- d
+  actualdat$vals_actual <-  predict(modified_fe_mod, newdata = actualdat)
+  actualdat$vals_counter <-  predict(modified_fe_mod, newdata = counterdat)
+  
+  results_dat_1980 <- actualdat %>% 
+    filter(year >= 1981 & year <= 1991) %>%  
+    mutate(tree_diff = vals_actual - vals_counter) %>% 
+    mutate(iteration = i, period = "1981-1991")
+  
+  results_dat_2000 <- actualdat %>% 
+    filter(year >= 1992 & year <= 2002) %>% 
+    mutate(tree_diff = vals_actual - vals_counter) %>% 
+    mutate(iteration = i, period = "1992-2002")
+  
+  results_dat_2010 <- actualdat %>% 
+    filter(year >= 2003 & year <= 2013) %>% 
+    mutate(tree_diff = vals_actual - vals_counter) %>% 
+    mutate(iteration = i, period = "2003-2013")
+  
+  results_precip <- rbind(results_precip, results_dat_1980, results_dat_2000, results_dat_2010)
+}
+
+head(results_precip)
+
+
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# calculations
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+meanchange_noprecip <- meanchange %>% 
+  mutate(Scenario = "Without precip counterfactual")
+  
+  
+precip_main <-  results_precip %>% 
+  group_by(period, iteration) %>% 
+  summarise(mean_diff = mean(tree_diff)) %>%
+  group_by(period) %>% 
+  summarize(lower_ci = round(quantile(mean_diff, 0.025), digits = 3),
+            rwi_mean = round(mean(mean_diff), digits=3),
+            upper_ci = round(quantile(mean_diff, 0.975), digits = 3)) %>% 
+  mutate(Scenario = "With precip counterfactual")
+
+
+comb_main_res <- precip_main %>% 
+  full_join(meanchange_noprecip)
+
+
+## figure
+comb_main_res %>% 
+  ggplot(aes(x = factor(period), y = rwi_mean, color = Scenario, fill = Scenario, shape = Scenario)) +
+  geom_hline(yintercept = 0, linetype = "dashed") +
+  geom_errorbar(aes(ymin = lower_ci, ymax = upper_ci, width = 0.07), 
+                position = position_dodge(width = 0.5), color = "black") +
+  geom_point(size = 3, position = position_dodge(width = 0.5))+ 
+  labs(title = "Estimated climate change impacts",
+       x = "Time period",
+       y = "Δ RWI") +
+  scale_color_manual(values = color_palette) +
+  scale_fill_manual(values = color_palette) 
+
+
+
+color_palette <- c("Without precip counterfactual" = "#303077", "With precip counterfactual" = "#8c6e87")
